c29336bcb200a133cd5c14e4816c79aa322f20a6
Lukas H <lukas.hajda@demi.sk>
2021-08-24 21:49:48 +0200

    Better performance

--- calculating_distance.js
+++ calculating_distance.js
@@ -1,4 +1,20 @@
 cy.on('drag', "node", function() {
-    let l = this.incomers('node').filter(h => h.id() !== this.id()).map(node => node.id());
-    console.log(l);
+    // let incomers = this.incomers('node').filter(h => h.id() !== this.id()).map(node => node.id());
+    // let outcomers = this.outgoers('node').filter(h => h.id() !== this.id()).map(node => node.id());
+    let e = this.connectedEdges(); // Dostanem vsetky hrany (in aj out)
+        console.log(e);
+    // console.log(incomers);
+    // console.log(outcomers);
+
+    // calculate_distance([incomers, outcomers], this);
 });
+
+// function calculate_distance(nodes_arr, given_node) {
+//     for (let set_nodes in nodes_arr) {
+//         for (let nodes in set_nodes) {
+//
+//         }
+//     }
+// }
+
+// function calculate()
