9341dce902dc79fb76a58985f9559c07410d06cb
Lukas Hajda <lukas.hajda@demi.sk>
2021-10-31 23:43:20 +0100

    Calculating-distance alpha (calculating-distance)

--- init_data.js
+++ init_data.js
@@ -1,8 +1,16 @@
 
 let nodes_positions = new Map();
+let edges_info = new Map();
 
 let ids = cy.nodes().map(node => node.data().name);
+let edges = cy.edges();
+
+edges.forEach(function (edge) {
+    edges_info.set(edge.data().source + edge.data().target, edge.data().weight);
+})
 
 ids.forEach(function (id, index) {
-    nodes_positions.set(id, [parseInt(cy.$('#' . id).position().x), parseInt(cy.$('#' . id).position().x)]);
+    nodes_positions.set(id, [parseInt(cy.$('#' + id).position().x), parseInt(cy.$('#' + id).position().x)]);
 });
+
+console.log(nodes_positions);
