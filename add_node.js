5b994c61204ef473ef9ca44f629fb0446f87fb2f
Lukas Hajda <lukas.hajda@demi.sk>
2021-11-13 20:12:18 +0100

    Remove comm (main)

--- add_node.js
+++ add_node.js
@@ -6,7 +6,6 @@ cy.on('tap', function(event){
         let yPos = event.position.y;
         console.log(xPos, yPos);
         this.add([{
-            // classes: 'automove-viewport',
             group: "nodes",
             data: { id: 'new' + Math.round( Math.random() * 100 ), label: 'new' + Math.round( Math.random() * 100 )},
             position: {
