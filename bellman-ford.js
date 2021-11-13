a0f60d75c39b4527cc61595eb0c9d3c8ea8cd2cf
Lukas Hajda <lukas.hajda@demi.sk>
2021-11-13 21:12:23 +0100

    Neg weight (bellman)

--- bellman-ford.js
+++ bellman-ford.js
@@ -5,31 +5,39 @@ let bf = cy.elements().bellmanFord({
         return edge.data().weight
     }
 });
+console.log(bf.hasNegativeWeightCycle);
+// TODO pcekovat ci je negativny cyklus ..ak neni tak pusti animaciu
+// let result = bf.pathTo('#H');
+console.log(bf.negativeWeightCycles);
+// let edges = result.edges();
+//
+//
+// let index = 0;
+// let length = edges.length;
+// if ( !result.hasNegativeWeightCycle ) {
+//     function runBellmanFord() {
+//
+//         if ( index >= length ){
+//             clearInterval(timer);
+//             return;
+//         }
+//
+//         let edge = edges[index++]
+//         if (edge === undefined) return;
+//         edge.animation({
+//             style: {
+//                 'line-color' : 'black',
+//                 'target-arrow-color': 'black'
+//             },
+//             duration: 500
+//         }).play();
+//
+//     }
+//
+//     let timer = setInterval(runBellmanFord, 1000);
+// } else {
+//     console.log('negative');
+// }
 
 
-let result = bf.pathTo('#H');
-let edges = result.edges();
 
-
-let index = 0;
-let length = edges.length;
-function runBellmanFord() {
-
-    if ( index >= length ){
-        clearInterval(timer);
-        return;
-    }
-
-    let edge = edges[index++]
-    if (edge === undefined) return;
-    edge.animation({
-        style: {
-            'line-color' : 'black',
-            'target-arrow-color': 'black'
-        },
-        duration: 500
-    }).play();
-
-}
-
-let timer = setInterval(runBellmanFord, 1000);
