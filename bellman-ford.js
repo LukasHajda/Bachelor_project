daccf66e5fe819be576630411d0b1c5f38966030
Lukas Hajda <lukas.hajda@demi.sk>
2021-11-20 20:43:11 +0100

    Not finished bellman (bellman)

--- bellman-ford.js
+++ bellman-ford.js
@@ -8,36 +8,33 @@ let bf = cy.elements().bellmanFord({
 console.log(bf.hasNegativeWeightCycle);
 // TODO pcekovat ci je negativny cyklus ..ak neni tak pusti animaciu
 // let result = bf.pathTo('#H');
-console.log(bf.negativeWeightCycles);
+// console.log(bf.negativeWeightCycles);
+// let result = bf.pathTo('#H');
 // let edges = result.edges();
-//
 //
 // let index = 0;
 // let length = edges.length;
-// if ( !result.hasNegativeWeightCycle ) {
-//     function runBellmanFord() {
+
+// function runBellmanFord() {
 //
-//         if ( index >= length ){
-//             clearInterval(timer);
-//             return;
-//         }
-//
-//         let edge = edges[index++]
-//         if (edge === undefined) return;
-//         edge.animation({
-//             style: {
-//                 'line-color' : 'black',
-//                 'target-arrow-color': 'black'
-//             },
-//             duration: 500
-//         }).play();
-//
+//     if ( index >= length ){
+//         clearInterval(timer);
+//         return;
 //     }
 //
-//     let timer = setInterval(runBellmanFord, 1000);
-// } else {
-//     console.log('negative');
+//     let edge = edges[index++]
+//     if (edge === undefined) return;
+//     edge.animation({
+//         style: {
+//             'line-color' : 'black',
+//             'target-arrow-color': 'black'
+//         },
+//         duration: 500
+//     }).play();
+//
 // }
+
+// let timer = bf.hasNegativeWeightCycle ? null : setInterval(runBellmanFord, 1000);
 
 
 
