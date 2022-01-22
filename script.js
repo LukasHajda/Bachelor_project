43ea379d85e019d158ed1b776168db25c2eca0ce
Lukas Hajda <lukas.hajda@demi.sk>
2022-01-22 20:36:27 +0100

    Add edge color (color)

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $(function () {
     
     let color1 = $('#color1');
     let color2 = $('#color2');
+    let color4 = $('#color4');
 
     $('.colorPick').change(function () {
         console.log('linear-gradient(' + color1.val() + ',' + color2.val() + ') no-repeat fixed');
@@ -15,6 +16,15 @@ $(function () {
 
     node_color.change(function () {
         cy.nodes().style('background-color', $(this).val());
+    });
+
+    color4.change(function () {
+        console.log('zmena');
+        cy.edges().style({
+            'line-color': $(this).val(),
+            'target-arrow-color' : $(this).val()
+        });
     })
+
     
 })
